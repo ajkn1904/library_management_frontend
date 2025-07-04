206c5775f933bf40b5087375200233d03ceea8c1
ajkn1904 <ajknishat@gmail.com>
2025-07-05 05:11:51 +0600

    footer updated

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,10 +1,31 @@
+import { Facebook, Twitter, Youtube } from "lucide-react";
+import { Link } from "react-router";
 
 const Footer = () => {
     return (
-        <div className='h-32 bg-gray-800 text-white flex items-center justify-center'>
-            This is Footer
-        </div>
+        <footer className="footer footer-center p-10 bg-gray-800 text-white">
+            <div className="flex flex-row justify-evenly items-center">
+            <div className="grid grid-flow-col gap-6">
+                <Link to='/' className="link link-hover">About us</Link>
+                <Link to='/' className="link link-hover">Contact</Link>
+                <Link to='/' className="link link-hover">Privacy & Policy</Link>
+            </div>
+            <div>
+                <div className="grid grid-flow-col gap-6">
+                    <a href='https://twitter.com/' target='_blank' rel="noreferrer"><Twitter className='w-6 h-6' /></a>
+
+                    <a href='https://www.youtube.com/' target='_blank' rel='noreferrer'><Youtube className='w-6 h-6' /></a>
+
+                    <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'><Facebook className='w-6 h-6' /></a>
+                </div>
+            </div>
+            </div>
+            <div className="text-center mt-5">
+                <small>© 2025 || Anika Jumana Khanam</small>
+            </div>
+        </footer>
     );
+
 };
 
 export default Footer;
